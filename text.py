49b7ce4db70d58d64d8988a61fdb30338a1db753
Amit191kumar <amitkumaryadav191@gmail.com>
2020-04-28 12:35:17 +0530

    tosc

---
+++ text.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from gtts import gTTS
+from playsound import playsound
+import os,uuid
+############################## FOR FILE NAME #####################################
+def filename():
+	
+		cdate=str(uuid.uuid4())+"sound.mp3"
+		return os.path.join(os.getcwd(),cdate)
+win=tk.Tk()
+win.title("text to speech")
+win.geometry("300x300")
+
+def text_to_speech():
+	text=entry.get()
+	speech=gTTS(text=text,lang='en')
+	filenameobj=filename()
+	speech.save(filenameobj)
+	playsound(filenameobj)
+
+label=tk.Label(win,text="Enter Here: ")
+label.grid(row=0,column=0)
+
+entry=tk.Entry(win)
+entry.grid(row=1,column=0)
+
+button=tk.Button(win,text="Go",command=text_to_speech)
+button.grid(row=1,column=1)
+win.mainloop()
